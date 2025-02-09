315d5c1a5a86752a5ee68f52925dfeb9dffa363c
Sherzod Abdurahmanov <sherzodrts@gmaill.com>
2025-02-09 16:12:35 +0300

    models are registered

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Home
+from .models import *
 
 admin.site.register(Home)
+admin.site.register(Stats)
+admin.site.register(TeamMember)
+admin.site.register(Blog)
+admin.site.register(BlogDetail)
+admin.site.register(Achievement)
+admin.site.register(FAQ)
+admin.site.register(Contact)
+
+
