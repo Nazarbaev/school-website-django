c795602963da5efd7640b2e7411e9342dc9e199e
Sherzod Abdurahmanov <sherzodrts@gmaill.com>
2025-03-08 18:32:41 +0300

    frontend changed

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,29 @@
 from django.conf.urls.static import static
-from django.urls import path
-
 from stem import settings
-from . import views
+from django.urls import path
+from .views import (IndexView,
+                    GalleryView,
+                    StarterPageView,
+                    BlogView,
+                    BlogDetailsView,
+                    CambridgeView,
+                    EdupageView,
+                    FaqView,
+                    AchievementsView,
+                    AchievementsDetailsView)
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", IndexView.as_view(), name="index"),
+    path("gallery/", GalleryView.as_view(), name="gallery"),
+    path("starter-page/", StarterPageView.as_view(), name="starter-page"),
+    path("blog/", BlogView.as_view(), name="blog"),
+    path("blog-details/", BlogDetailsView.as_view(), name="blog-details"),
+    path("cambridge/", CambridgeView.as_view(), name="cambridge"),
+    path("edupage/", EdupageView.as_view(), name="edupage"),
+    path("faq/", FaqView.as_view(), name="faq"),
+    path("achievements/", AchievementsView.as_view(), name="achievements"),
+    path("achievements-details/", AchievementsDetailsView.as_view(), name="achievements-details")
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,41 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def index(request):
-    return render(request, "index.html")
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+class GalleryView(TemplateView):
+    template_name = "gallery.html"
+
+
+class StarterPageView(TemplateView):
+    template_name = "starter-page.html"
+
+
+class BlogView(TemplateView):
+    template_name = "blog.html"
+
+
+class BlogDetailsView(TemplateView):
+    template_name = "blog-details.html"
+
+
+class CambridgeView(TemplateView):
+    template_name = "cambridge.html"
+
+
+class EdupageView(TemplateView):
+    template_name = "edupage.html"
+
+
+class FaqView(TemplateView):
+    template_name = "FAQ.html"
+
+
+class AchievementsView(TemplateView):
+    template_name = "achievements.html"
+
+
+class AchievementsDetailsView(TemplateView):
+    template_name = "achievements-details.html"
